9cc52c463f6cf6330c0faedc17251dcddedadde4
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-01-10 22:31:08 +0000

    uploading...

--- E-Associations/src/dao/SingletonConnection.java
+++ E-Associations/src/dao/SingletonConnection.java
@@ -8,10 +8,11 @@ public class SingletonConnection {
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/e_association","root","");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/e-association","root","");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
+			System.out.println("Killer hell Killer");
 		}
 	}
 	public static Connection getConnection() {
