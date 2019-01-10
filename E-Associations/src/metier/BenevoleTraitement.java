048dafed93b7ea475ffe73dccd787cfac3effbac
MariamMach <39934426+MariamMach@users.noreply.github.com>
2019-01-10 20:51:29 +0000

    Fifth upload

--- E-Associations/src/metier/BenevoleTraitement.java
+++ E-Associations/src/metier/BenevoleTraitement.java
@@ -1,6 +1,37 @@
 package metier;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import dao.SingletonConnection;
+import web.BenevoleModel;
+
+
 public class BenevoleTraitement {
+	
+	private static Connection conx = SingletonConnection.getConnection();
+	
+	public static BenevoleModel ChercherBenevoleCin(String cin){
+		BenevoleModel benevole = new BenevoleModel();
+		
+		try {
+			
+			PreparedStatement ps;
+			ps = (PreparedStatement) conx.prepareStatement("select * from benevole where cin = ?");
+			ps.setString(1, cin);		
+			ResultSet rs = 	ps.executeQuery();
+			while(rs.next()) {
+				benevole = new BenevoleModel(rs.getString("	cin"), rs.getString("nom_ben"), rs.getString("prenom_ben"),
+						rs.getString("profession_ben"), rs.getString("email_ben"), rs.getString("tele_ben") , rs.getString("sexe_ben") ,rs.getInt("id_authentif") );
+			}
+			ps.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+		
+		return benevole;
+	}
 	
 
 }
